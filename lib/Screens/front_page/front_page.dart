eededf10a023074f37d4af904ef91107e4c26503
prajesh9921 <prajeshgawhale2019.it@mmcoe.edu.in>
2021-05-11 10:46:30 +0530

    added project

---
+++ lib/Screens/front_page/front_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Front_page extends StatefulWidget {
+  @override
+  _Front_pageState createState() => _Front_pageState();
+}
+
+class _Front_pageState extends State<Front_page> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
+}
