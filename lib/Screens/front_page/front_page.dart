ea8cd5ab9b051366b095e5b8c57d4b501b74763b
Hrishikesh Adsod <hrishikesh.adsod@gmail.com>
2021-05-11 18:22:52 +0530

    first screen update

--- lib/Screens/front_page/front_page.dart
+++ lib/Screens/front_page/front_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import 'HorizontalList.dart';
 class Front_page extends StatefulWidget {
   @override
   _Front_pageState createState() => _Front_pageState();
@@ -9,7 +10,13 @@ class _Front_pageState extends State<Front_page> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      body:ListView(
+        children: <Widget>[
+          Padding(padding: EdgeInsets.all(20),
+          child: Text('My List',style: TextStyle(fontWeight:FontWeight.bold,fontSize: 18),),),
+          MyList(),
+        ],
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:shopping/Screens/front_page/front_page.dart';
-
+import 'package:flutter/services.dart';
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
   runApp(MyApp());
 }
 
@@ -10,10 +13,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
       home: Front_page()
     );
   }
